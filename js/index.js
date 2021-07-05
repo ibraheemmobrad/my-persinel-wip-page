b21ef7c796fa2bae1662dfa828f231dba42f0885
ibraheemmobrad <62356577+ibraheemmobrad@users.noreply.github.com>
2021-07-06 02:58:25 +0300

    update progekt

--- js/index.js
+++ js/index.js
@@ -1,14 +1,10 @@
-// function addNewz(newz_head, newz_details) {
-//   var newzHead = document.createElement("h3");
-//   var newzHeadText = document.createTextNode(newzHead)
-//   newzHead.appendChild(newzHeadText)
-//   var newzDetails = document.createElement("p");
-//   var newzDetailsText = document.createTextNode(newzHead)
-//   newzDetails.appendChild(newzDetailsText)
-//   document.getElementById("newzKontainer").appendChild(newzHead,newzDetails)
-// }
-// // addNewz ("hi","this is a test newz")
-// function alerts(text) {
-//   alert (alerts);
-// };
-// alerts ("hi");
+function addNewz(newz_head, newz_details) {
+  var newzHead = document.createElement("h3");
+  var newzHeadText = document.createTextNode(newzHead);
+  newzHead.appendChild(newzHeadText);
+  var newzDetails = document.createElement("p");
+  var newzDetailsText = document.createTextNode(newzHead);
+  newzDetails.appendChild(newzDetailsText);
+  document.body.appendChild(newzHead, newzDetails);
+};
+addNewz ("hi","this is a test newz")
